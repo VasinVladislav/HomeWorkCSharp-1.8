4d0f995bcc665c91ef628da5937a144df1a961a7
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 12:33:56 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.8. Составить программу вывода на экран в одну строку четырех любых чисел с одним пробелом между ними.
+            Random rnd = new Random();
+            int valueOne = rnd.Next();
+            int valueTwo = rnd.Next();
+            int valueThree = rnd.Next();
+            int valueFour = rnd.Next();
+            Console.WriteLine($"{valueOne}{valueTwo} {valueThree}{valueFour}");
+        }
+    }
+}
